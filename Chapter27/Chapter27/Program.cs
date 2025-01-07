31dd0d40c0a69b8d7b9856e9ba1fd7347c36f9ff
Chanwoong <cksdnddlek99@gmail.com>
2025-01-07 12:13:04 +0900

    add: 자바와 사용법이 다른 Dictionary(Map) 학습

---
+++ Chapter27/Chapter27/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class C
+{
+    static void Main()
+    {
+        Dictionary<string, int> map = new Dictionary<string, int>();
+        map.Add("a", 1);
+        map.Add("b", 2);
+
+        foreach (var key in map.Keys)
+        {
+            Console.WriteLine("" + key + " " + map[key]);
+        }
+
+        map["a"] = 3;
+        map["b"] = 4;
+        foreach (var key in map.Keys)
+        {
+            Console.WriteLine("" + key + " " + map[key]);
+        }
+    }
+}
